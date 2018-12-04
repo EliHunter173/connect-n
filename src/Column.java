5f0e06001c0b2838815047dceee3c41e330234e9
Eli W. Hunter <hunter.eli.w@gmail.com>
2018-12-04 10:41:34 -0500

    Finish (untested) Column.java

--- src/Column.java
+++ src/Column.java
@@ -1,19 +1,31 @@
 public class Column {
 
+    private final int maxTokens;
     private Token[] tokens;
     private int numberOfTokens;
-    private final int maxTokens;
 
     public int getMaxTokens() {
+        return maxTokens;
+    }
 
+    public Column(int height) {
+        this.maxTokens = height;
+        this.tokens = new Token[height];
+        this.numberOfTokens = 0;
     }
 
     public Token getToken(int row) {
-
+        if (row < 0 || row >= maxTokens) {
+            throw new IllegalArgumentException("Invalid row");
+        }
+        return tokens[row];
     }
 
     public void addToken(Token token) {
-
+        if (numberOfTokens >= maxTokens) {
+            throw new IllegalArgumentException("Column is full");
+        }
+        tokens[numberOfTokens++] = token;
     }
 
 }
